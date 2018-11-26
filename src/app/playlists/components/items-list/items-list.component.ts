21fae000f2441710aaa2c15f51497ab6770f68cb
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-26 12:05:38 +0100

    style encapsulation, ng-deep, host, host-context

--- src/app/playlists/components/items-list/items-list.component.ts
+++ src/app/playlists/components/items-list/items-list.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
-  selector: 'app-items-list',
-  templateUrl: './items-list.component.html',
-  styleUrls: ['./items-list.component.scss']
+  selector: "app-items-list",
+  templateUrl: "./items-list.component.html",
+  styleUrls: ["./items-list.component.scss"]
+  // encapsulation: ViewEncapsulation.Emulated
 })
 export class ItemsListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
