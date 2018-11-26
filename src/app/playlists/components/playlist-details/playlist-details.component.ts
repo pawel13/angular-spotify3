4c854cce9b1c0b50c870cd1d027b3558cf025180
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-26 15:02:09 +0100

    hidden

--- src/app/playlists/components/playlist-details/playlist-details.component.ts
+++ src/app/playlists/components/playlist-details/playlist-details.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Playlist } from 'src/app/model/Playlist';
+import { Component, OnInit } from "@angular/core";
+import { Playlist } from "src/app/model/Playlist";
 
 @Component({
-  selector: 'app-playlist-details',
-  templateUrl: './playlist-details.component.html',
-  styleUrls: ['./playlist-details.component.scss']
+  selector: "app-playlist-details",
+  templateUrl: "./playlist-details.component.html",
+  styleUrls: ["./playlist-details.component.scss"]
 })
 export class PlaylistDetailsComponent implements OnInit {
+  playlist: Playlist = {
+    id: 123,
+    name: "Angular Hits",
+    favourite: true,
+    color: "#ff00ff"
+  };
 
-  playlist:Playlist = {
-    id:123,
-    name:'Angular Hits',
-    favourite:true,
-    color:'#ff00ff',
+  constructor() {}
+
+  ngOnInit() {}
+
+  mode = "show";
+
+  edit() {
+    this.mode = "edit";
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  cancel() {
+    this.mode = "show";
   }
 
+  save() {
+    this.mode = "show";
+  }
 }
