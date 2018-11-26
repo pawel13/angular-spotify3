18a47c19fc067b7fb0f3b6ff662efc6473d606b2
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-26 15:55:40 +0100

    ngFor ngForOf $ implicit let i = index ngForOfContext

--- src/app/playlists/components/items-list/items-list.component.ts
+++ src/app/playlists/components/items-list/items-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Playlist } from "src/app/model/Playlist";
 
 @Component({
   selector: "app-items-list",
@@ -7,6 +8,29 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
   // encapsulation: ViewEncapsulation.Emulated
 })
 export class ItemsListComponent implements OnInit {
+
+  playlists: Playlist[] = [
+    {
+      id: 123,
+      name: "Angular Hits",
+      favourite: true,
+      color: "#ff00ff"
+    },
+    {
+      id: 234,
+      name: "Angular TOP20",
+      favourite: false,
+      color: "#00ffff"
+    },
+    {
+      id: 345,
+      name: "Best of Angular",
+      favourite: false,
+      color: "#ffff00"
+    },
+  ];
+
+
   constructor() {}
 
   ngOnInit() {}
