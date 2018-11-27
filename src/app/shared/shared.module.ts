999cc788c86da7656ee2db549163cb33e8615633
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-27 15:48:27 +0100

    Tabs nav

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,13 @@ import { UnlessDirective } from './unless.directive';
 import { CardComponent } from './card/card.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { TabComponent } from './tab/tab.component';
+import { TabsNavComponent } from './tabs-nav/tabs-nav.component';
 
 @NgModule({
-  declarations: [HighlightDirective, UnlessDirective, CardComponent, TabsComponent, TabComponent],
+  declarations: [HighlightDirective, UnlessDirective, CardComponent, TabsComponent, TabComponent, TabsNavComponent],
   imports: [
     CommonModule
   ],
-  exports: [HighlightDirective, UnlessDirective, CardComponent, TabsComponent, TabComponent]
+  exports: [HighlightDirective, UnlessDirective, CardComponent, TabsComponent, TabComponent, TabsNavComponent]
 })
 export class SharedModule { }
