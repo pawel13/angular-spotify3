2e7255012be27ff91afb288656ba3011f2259edc
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-27 11:58:58 +0100

    Directive lifecycle, onINit, onChanges, DoCheck, OnDestroy

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -1,12 +1,41 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  OnDestroy,
+  SimpleChanges
+} from "@angular/core";
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: "[appHighlight]"
 })
-export class HighlightDirective {
+export class HighlightDirective
+  implements OnInit, OnChanges, DoCheck, OnDestroy {
 
-  constructor() { 
-    console.log('hello!')
+  @Input("appHighlight")
+  color: string;
+
+  constructor(private elem: ElementRef<HTMLElement>) {
+    console.log("constructor!");
   }
 
+  ngOnInit() {
+    console.log("ngOnInit!");
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges", changes);
+    this.elem.nativeElement.style.color = this.color;
+  }
+  
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy");
+  }
 }
