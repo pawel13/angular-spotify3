88853b8d7c48ee1d30ef701c419e77af34c14794
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-29 15:53:55 +0100

    simple routing, redirectTo, pathMatch, path:** - 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PlaylistsViewComponent } from "./playlists/components/playlists-view/playlists-view.component";
+import { MusicSearchComponent } from "./music/components/music-search/music-search.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    // component: HomeComponent,
+    redirectTo: "playlists",
+    pathMatch: "full"
+  },
+  {
+    path: "playlists",
+    component: PlaylistsViewComponent
+  },
+  {
+    path: "music",
+    component: MusicSearchComponent
+  },
+  {
+    path: "**",
+    // component: PageNotFoundComponent,
+    redirectTo: "playlists",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: true,
+      useHash: true
+    })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
