577f7b41bb130c6907e00647f6cd5f0778107677
Mateusz Kulesza <eduwebgit@gmail.com>
2018-11-30 09:27:46 +0100

    multicast (()=>new Subject) + connect(), refCount, share, shareReply

--- src/app/music/components/album-details/album-details.component.ts
+++ src/app/music/components/album-details/album-details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { AlbumsService } from "../../services/albums.service";
 import { ActivatedRoute } from "@angular/router";
-import { map, filter, switchMap } from "rxjs/operators";
+import { map, filter, switchMap, multicast, refCount, share, shareReplay } from "rxjs/operators";
+import { Subject, ConnectableObservable } from "rxjs";
+import { Album } from "src/app/model/Album";
 
 @Component({
   selector: "app-album-details",
@@ -13,12 +15,24 @@ export class AlbumDetailsComponent implements OnInit {
   album$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get("id")),
     filter(id => id !== null),
-    switchMap(id => this.service.getAlbum(id!))
+    switchMap(id => this.service.getAlbum(id!)),
+    // multicast(()=>new Subject()),
+    // refCount() // auto-connect on first subscriber
+    // share() // multicast + ()=>Subject + refCount
+    shareReplay() // multicast + ()=>ReplaySubject + refCount
   );
 
   constructor(
     private route: ActivatedRoute, 
     private service: AlbumsService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    
+    // setInterval(()=>{
+    //   (this.album$ as ConnectableObservable<Album>).connect()
+
+    // },3000)
+
+
+  }
 }
